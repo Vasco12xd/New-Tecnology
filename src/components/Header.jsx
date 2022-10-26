88b26a2871eadf730c1a7cf4629413fd4c4b10bb
Esteban Vasco <evasco.27@hotmail.com>
2022-10-25 22:58:57 -0500

    Color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
+    
     <header>
       <nav className="navbar navbar-expand-lg justify-content-between mb-5">
         <div className="container">
           <h1>
-            <Link to={"/"} className="text-light text-decoration-none">
+            <Link to={"/"} className="text-decoration-none">
               Redux
             </Link>
           </h1>
